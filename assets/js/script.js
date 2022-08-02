5737d403aeb7cb10beb09fdec1452e1f9535699d
maria almonacid <almonacid.maria@gmail.com>
2022-08-02 15:39:32 -0400

    desafiocreandoymodiciandoobjetos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,140 @@
-$(()=>  {
-  let expresion = /\d/;
 
-  let consulta = (hero) => {
-    if (hero && expresion.test(hero)) {
-      $.ajax({
-        dataType: "json",
-        type: "get",
-        url: `https://www.superheroapi.com/api.php/3525635500807579/${hero}`,
-        success: (result) => {
-          if (result.response === "success") {
-            let resultado1 = `
-            <h3 class="text-center">SuperHero Encontrado</h3>
-            <div class="card">
-              <div class="row no-gutters">
-                <div class="col-md-4">
-                  <img src="${result.image.url}" class="card-img" alt="${result.name}">
-                </div>
-                <div class="col-md-8">
-                  <div class="card-body">
-                    <h5 class="card-title">Nombre: ${result.name}</h5>
-                    <p class="card-text">Conecciones: ${result.connections["group-affiliation"]}</p>
-                    <ul class="list-group list-group-flush">
-                      <li class="list-group-item"><em>Publicado por:</em>: ${result.biography.publisher}</li>
-                      <li class="list-group-item"><em>Ocupación:</em> ${result.work.occupation}</li>
-                      <li class="list-group-item"><em>Primera Aparición:</em> ${result.biography["first-appearance"]}</li>
-                      <li class="list-group-item"><em>Altura:</em> ${result.appearance.height.join(" - ")}.</li>
-                      <li class="list-group-item"><em>Peso:</em> ${result.appearance.weight.join( " - " )}.</li>
-                      <li class="list-group-item"><em>Alianzas:</em> `;
-                        let resultado2 = "";
-                        result.biography.aliases.forEach((alianzas) => {
-                        resultado2 += `
-                        <span>${alianzas}</span>
-                        `;
-                        });
-                        let resultado3 = `
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </div>
-            </div>
-            `;
-            $("#resultado").append(resultado1 + resultado2 + resultado3);
+  // constructor consultorio 
 
-            let datosXY = [];
-            for (const key in result.powerstats) {
-              datosXY.push({
-                label: key,
-                y: parseInt(result.powerstats[key]),
-              });
+  function Consultorio(nombre, paciente) {
+    var _nombre = nombre;
+    var _paciente = paciente || [];
+
+    Object.defineProperty(this, "getNombre", {
+        get: function () {
+            return _nombre;
+        }
+    })
+     Object.defineProperty(this, "setNombre", {
+        set: function (nombre) {
+            _nombre = nombre;
+        }
+    })
+
+    Object.defineProperty(this, "getPaciente", {
+        get: function () {
+            return _paciente;
+        }
+    })
+     Object.defineProperty(this, "setPaciente", {
+        set: function (paciente) {
+            if(Array.isArray(_paciente)){
+              _paciente.push(paciente);
+            }else{
+              _paciente = [_paciente];
+              _paciente.push(paciente);
             }
+        }
+    })
+}
 
-            var options = {
-              title: {
-                text: `Estadísticas de Poder ${result.name}`,
-              },
-              data: [
-                {
-                  type: "pie",
-                  startAngle: 45,
-                  showInLegend: "true",
-                  legendText: "{label}",
-                  indexLabel: "{label} ({y})",
-                  yValueFormatString: "#,##0.#" % "",
-                  dataPoints: datosXY,
-                },
-              ],
-            };
-//grafico
-            $("#chartContainer").CanvasJSChart(options);
-          } else {
-            alert("ID no encontrado");
-          }
-          $("#hero").val("");
-        },
-        error: () => {
-          alert("Error para esta consulta");
-        },
-      });
-    } else {
-      alert("Ingrese solo números");
-    }
-  };
+// constructor paciente
+function Paciente(nombre, edad, rut, diagnostico) {
+    var _nombre = nombre;
+    var _edad = edad;
+    var _rut = rut;
+    var _diagnostico = diagnostico;
+    Object.defineProperty(this, "getNombre", {
+        get: function () {
+            return _nombre
+        }
+    }) 
+     Object.defineProperty(this, "setNombre", {
+        set: function (nombre) {
+            _nombre = nombre
+        }
+    }) 
+    Object.defineProperty(this, "getEdad", {
+        get: function () {
+            return _edad
+        }
+    })
+    Object.defineProperty(this, "setEdad", {
+        set: function (edad) {
+            _edad = edad
+        }
 
-  $("form").on("submit", (event) => {
-    event.preventDefault();
+    })
+    Object.defineProperty(this, "getRut", {
+        get: function () {
+            return _rut
+        }
+    })
+    Object.defineProperty(this, "setRut", {
+        set: function (rut) {
+            _rut = rut
+        }
+    })
+    Object.defineProperty(this, "getDiagnostico", {
+        get: function () {
+            return _diagnostico
+        }
+    })
+    Object.defineProperty(this, "setDiagnostico", {
+        set: function (diagnostico) {
+            _diagnostico = diagnostico;
+        }
+    })
+}
 
-    $("#resultado").html(" ");
-    $("#chartContainer").html(" ");
-    hero = parseInt($("#hero").val());
-    consulta(hero);
-    //document.getElementById("hero").reset(); 
-    
+
+
+var paciente_1 = new Paciente("Mario Delgado", "32", "12.998.878-0", "diarrea cronica");
+var paciente_2 = new Paciente("Matias Perez", "45", "12.956.978-0", "difteria");
+var paciente_3 = new Paciente("Pedro Fuentes", "56", "10.776.878-0", "Covid-19(Sars-Cov2)");
+var paciente_4 = new Paciente("Marcia Contreras", "62", "5.776.878-0", "torticolis");
+
+var consultorio_1 = new Consultorio("Doña Juana", [paciente_1, paciente_2,paciente_3,paciente_4]);
+
+
+Consultorio.prototype.agregarPacientes = function (paciente) {
+  this.setPaciente = paciente;
+}
+
+
+Consultorio.prototype.getNombreConsultorio = function () {
+    return this.getNombre;
+}
+Paciente.prototype.getNombrePaciente = function () {
+    return this.getNombre;
+}
+
+
+
+Consultorio.prototype.mostrarDatos = function () {
+    this.getPaciente.forEach(function (paciente, index) {
+        console.log(`${index + 1}.- nombre: ${paciente.getNombre}`);
+    })
+}
+
+Consultorio.prototype.buscarPaciente = function (nombre) {
   
-  });
+  var found = this.getPaciente.filter(function(paciente){
+    return paciente.getNombre == nombre;
+  })
+
+  console.log(found)
+  if(found.length > 0){
+    found.forEach(function(paciente, index){
+      console.log(`${index+1}.- Nombre: ${paciente.getNombre}`)
+    })
+  }else{
+    console.log("Paciente no encontrado")
+  }
 
   
+}
 
-});
+
+
+consultorio_1.mostrarDatos();
+
+consultorio_1.buscarPaciente("Matias Perez");
+
+
+
